4b1fe3a629ebd12df4b8b44707bbc3f50c596398
Dave Fancher <david.fancher@springfieldpublications.com>
2017-04-13 11:59:05 -0400

    Wired up details functionality

--- ElevenNote.Web/Controllers/NoteController.cs
+++ ElevenNote.Web/Controllers/NoteController.cs
@@ -43,6 +43,14 @@ namespace ElevenNote.Web.Controllers
             return View(model);
         }
 
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+
         private NoteService CreateNoteService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
